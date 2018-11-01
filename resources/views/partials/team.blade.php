a4288d60bcadb61f03e2220038d2a208cecccc9a
Nikolaj Buus <nbuuskofoed@gmail.com>
2018-11-01 21:44:04 +0100

    added items align center for images

--- resources/views/partials/team.blade.php
+++ resources/views/partials/team.blade.php
@@ -1,6 +1,6 @@
 @foreach($get_team_sections as $index => $section)
   <div class="row align-items-center py-5 @if($index % 2 != 0) flex-row-reverse bg-background @endif">
-    <div class="col-12 col-md-6 px-5 d-flex justify-content-center">
+    <div class="col-12 col-md-6 px-5 d-flex justify-content-center align-items-center">
       <img class="img-fluid" src="{{ $section['image']['url'] }}">
     </div>
     <div class="col-12 col-md-6 px-5">
