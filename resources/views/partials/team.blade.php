f528a752d5cf2f61dcf967f0a6f1aec1fdbac7a3
Nikolaj Buus <nbuuskofoed@gmail.com>
2018-11-06 22:39:03 +0100

    forside mere

--- resources/views/partials/team.blade.php
+++ resources/views/partials/team.blade.php
@@ -1,9 +1,9 @@
 @foreach($get_team_sections as $index => $section)
   <div class="row align-items-center py-5 @if($index % 2 != 0) flex-row-reverse bg-background @endif">
-    <div class="col-12 col-md-6 px-5 d-flex justify-content-center align-items-center">
+    <div class="col-12 col-md-6 px-6 d-flex justify-content-center align-items-center">
       <img class="img-fluid" src="{{ $section['image']['url'] }}">
     </div>
-    <div class="col-12 col-md-6 px-5">
+    <div class="col-12 col-md-6 px-6">
       {!! $section['text'] !!}
     </div>
   </div>
