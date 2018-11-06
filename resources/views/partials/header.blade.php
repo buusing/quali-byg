fc12ca9218b78f6391b9dcb6d1932fc8b75bfa12
Nikolaj Buus <nbuuskofoed@gmail.com>
2018-11-06 22:06:52 +0100

    fixed padding in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
-<div class="top-nav d-flex justify-content-between px-5">
+<div class="top-nav d-flex justify-content-between px-2 px-lg-5">
   <a href="mailto:qb@quali-byg.dk"><i class="far fa-envelope" style="margin-right: 10px"></i>qb@quali-byg.dk</a>
   <p><i class="fas fa-phone fa-rotate-90" style="margin-right: 10px"></i>Man-Frekl.09.00-15.30</p>
-  <p class="hidden lg:block"><a href="https://facebook.com"><i class="fab fa-facebook"></i></a><a href="https://instagram.com"><i class="fab fa-instagram"></i></a></p>
+  <p class="d-none d-lg-block"><a href="https://facebook.com"><i class="fab fa-facebook"></i></a><a href="https://instagram.com"><i class="fab fa-instagram"></i></a></p>
 </div>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
