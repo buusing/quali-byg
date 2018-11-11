237738ff10e6aeb8d01aee0e926becc0136d8569
Nikolaj Buus <nbuuskofoed@gmail.com>
2018-11-11 22:33:04 +0100

    enclosed everything in container

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -11,65 +11,67 @@
     </div>
   </div>
 </div>
-<div class="row om-os">
-  <div id="om-os" class="col-12 col-md-8 offset-md-2 py-5">
-    @if( have_posts() ) @php the_post() @endphp
-      {{ the_content() }}
-    @endif 
+<div class="container-fluid">
+  <div class="row om-os">
+    <div id="om-os" class="col-12 col-md-8 offset-md-2 py-5">
+      @if( have_posts() ) @php the_post() @endphp
+        {{ the_content() }}
+      @endif 
+    </div>
+    <div class="col-12">
+      @if($image = get_field('stort_billede'))
+        <img class="img-fluid" src="{{ $image['url'] }}">
+      @endif
+    </div>
   </div>
-  <div class="col-12">
-    @if($image = get_field('stort_billede'))
-      <img class="img-fluid" src="{{ $image['url'] }}">
-    @endif
-  </div>
-</div>
-@include('partials.team')
-<div class="row">
-  <div class="col-12 col-md-6 offset-md-3 text-center">
-    <h2 class="py-5 font-thin">Hvad siger vores kunder</h2>
-  </div>
-  <div class="col-12 no-padding">
-    @php
-      $query = new WP_Query(array(
-        'post_type' => 'anmeldelser',
-      ));
-    @endphp
-    @if($query->have_posts())
-      <div class="swiper-container">
-        <!-- Additional required wrapper -->
-        <div class="swiper-wrapper">
-          @while( $query->have_posts() ) @php $query->the_post() @endphp
-            <div class="swiper-slide d-flex justify-content-end align-items-center flex-wrap">
-              @if( has_post_thumbnail() )
-                <div class="slider-image" style="background-image: url({{ the_post_thumbnail_url() }});"></div>
-                <div class="col-12 d-md-none">
-                  <img class="img-fluid" src="{{ the_post_thumbnail_url() }}">
+  @include('partials.team')
+  <div class="row">
+    <div class="col-12 col-md-6 offset-md-3 text-center">
+      <h2 class="py-5 font-thin">Hvad siger vores kunder</h2>
+    </div>
+    <div class="col-12 no-padding">
+      @php
+        $query = new WP_Query(array(
+          'post_type' => 'anmeldelser',
+        ));
+      @endphp
+      @if($query->have_posts())
+        <div class="swiper-container">
+          <!-- Additional required wrapper -->
+          <div class="swiper-wrapper">
+            @while( $query->have_posts() ) @php $query->the_post() @endphp
+              <div class="swiper-slide d-flex justify-content-end align-items-center flex-wrap">
+                @if( has_post_thumbnail() )
+                  <div class="slider-image" style="background-image: url({{ the_post_thumbnail_url() }});"></div>
+                  <div class="col-12 d-md-none">
+                    <img class="img-fluid" src="{{ the_post_thumbnail_url() }}">
+                  </div>
+                @endif
+                <div class="col-12 col-md-6 slider-text mb-3 px-5 pr-md-5">
+                  {{ the_content() }}
+                  <p class="text-center">{{ the_title() }}</p>
                 </div>
-              @endif
-              <div class="col-12 col-md-6 slider-text mb-3 px-5 pr-md-5">
-                {{ the_content() }}
-                <p class="text-center">{{ the_title() }}</p>
               </div>
-            </div>
-          @endwhile
-        </div>
+            @endwhile
+          </div>
 
-        <!-- If we need navigation buttons -->
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
-      </div><!-- End slider -->
-    @endif
+          <!-- If we need navigation buttons -->
+          <div class="swiper-button-prev"></div>
+          <div class="swiper-button-next"></div>
+        </div><!-- End slider -->
+      @endif
+    </div>
   </div>
-</div>
-<div class="row py-5">
-</div>
-<div class="row contact-form">
-  <div class="col-12 col-md-6 offset-md-3 text-center">
-    <h3 class="h2">Ønsker du at vide mere?</h3>
-    <p>Send os en forespørsmål med dine ønsker og behov, så vender vi snarest tilbage til dig.</p>
+  <div class="row py-5">
   </div>
-  <div class="col-12 col-md-4 offset-md-4">
-    @php echo do_shortcode($contact_form) @endphp
+  <div class="row contact-form">
+    <div class="col-12 col-md-6 offset-md-3 text-center">
+      <h3 class="h2">Ønsker du at vide mere?</h3>
+      <p>Send os en forespørsmål med dine ønsker og behov, så vender vi snarest tilbage til dig.</p>
+    </div>
+    <div class="col-12 col-md-4 offset-md-4">
+      @php echo do_shortcode($contact_form) @endphp
+    </div>
   </div>
 </div>
 
